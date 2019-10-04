e794b74ecfc62d1539d966e1c208ca4da9ba45f3
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-04 10:58:40 +0700

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h2>This is a project of university</h2>
+      <h2>update</h2>
     </div>
   );
 }
