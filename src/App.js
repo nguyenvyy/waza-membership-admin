dfb2583a03890e27d20d5c5dd3e7971a0968a3db
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-09 11:20:56 +0700

    feature login page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css';
 import { LoginPage } from './pages/LoginPage/LoginPage';
+import { Admin } from './layouts/Admin';
 
 function App() {
   return (
     <div className="App container-full">
       <Router>
         <LoginPage />
+        <Admin />
       </Router>
     </div>
   );
 }
 
 export default App;
+
+export function confirmPassword(password) {
+  let result  = true;
+  if(password.length >= 6) return false
+  return result;
+
+}
