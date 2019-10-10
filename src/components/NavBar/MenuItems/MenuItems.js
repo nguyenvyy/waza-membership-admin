cc44942655a2979376367ffd3ad57f586c1e6a8c
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-10 22:42:51 +0700

    fix MenuItems component: add className

--- src/components/NavBar/MenuItems/MenuItems.js
+++ src/components/NavBar/MenuItems/MenuItems.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const MenuItems = ({items, collapsed}) => (
-    <ul>
+export const MenuItems = ({ className, items, collapsed}) => (
+    <ul className={className}>
         {items.map(({Component, title, to, items, ...rest}, index) => (
             <Component  title={title} to={to} items={items} key={index} collapsed={collapsed} {...rest} />
         ))}
