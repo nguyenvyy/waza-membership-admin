be6315e64f554505446aeb41f6ea3241efce7940
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-11 22:27:21 +0700

    refactor MenuItem component

--- src/components/NavBar/MenuItem/MenuItem.js
+++ src/components/NavBar/MenuItem/MenuItem.js
@@ -4,36 +4,36 @@ import { NavLink } from 'react-router-dom';
 import './MenuItem.css'
 import { LetterIcon } from '../LetterIcon/LetterIcon';
 
-export const MenuItem = ({ title, shortHand, to, collapsed, CollapsedIcon = LetterIcon }) => {
-    const [isActive, setIsActive] = useState(false) 
-    const realLayout = collapsed ? 'd-flex-center' : 'd-flex justify-content-start '
-    return (
-        <li>
-            <NavLink
+export const MenuItem = React.memo(
+    ({ title, to, collapsed, shortHand, CollapsedIcon = LetterIcon }) => {
+        const [isActive, setIsActive] = useState(false)
+        const realLayout = collapsed ? 'd-flex-center' : 'd-flex justify-content-start'
+        return (
+            <li>
+                <NavLink
                     to={{
                         pathname: to
                     }}
-                    isActive={(match) =>{
-                        if(match) 
+                    isActive={(match) => {
+                        if (match)
                             setIsActive(true)
                         else
                             setIsActive(false)
                     }}
-                    >
-                <div className={`${realLayout} ${isActive && 'actived'}  menu-item`}>
-                    {
-                        (shortHand !== undefined && collapsed === true) &&
-                        <CollapsedIcon title={title} isActive={isActive}>{shortHand}</CollapsedIcon>
-                        
-                    }
-                    {!collapsed && 
-                        <div className="title d-flex align-items-center">
-                            {title}
-                        </div>
-                    }
-
-                </div>
-            </NavLink>
-        </li>
-    )
-}
+                >
+                    <div className={`${realLayout} ${isActive && 'actived'}  menu-item`}>
+                        {
+                            collapsed === true ? (
+                                <CollapsedIcon title={title} isActive={isActive}>{shortHand}</CollapsedIcon>
+                            ) : (
+                                    <div className="title d-flex align-items-center">
+                                        {title}
+                                    </div>
+                                )
+                        }
+                    </div>
+                </NavLink>
+            </li>
+        )
+    }
+)
