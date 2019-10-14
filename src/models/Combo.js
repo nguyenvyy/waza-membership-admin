45ed0d7d70f3b2d10c92c07d54b28374d37a2910
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-14 22:38:19 +0700

    add Combo model

---
+++ src/models/Combo.js
@@ -0,0 +1,12 @@
+export  class Combo {
+    constructor(id, value, state, fromDate, toDate, isDeleted, voucherIdArray, description) {
+        this.id = id
+        this.value = value
+        this.state = state
+        this.fromDate = fromDate
+        this.toDate = toDate
+        this.isDeleted = isDeleted
+        this.voucherIdArray = voucherIdArray
+        this.description = description
+    }
+}
