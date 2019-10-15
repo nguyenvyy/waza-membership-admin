b3ee0ec5c3618fd03eba62512ed603f9d7c56eff
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-15 23:55:59 +0700

    update ActiveCombo

--- src/components/Combo/ActiveCombo/ActiveCombo.js
+++ src/components/Combo/ActiveCombo/ActiveCombo.js
@@ -1,65 +1,128 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import uuid from 'uuid'
-import { Table, Divider } from 'antd';
+import { Link } from 'react-router-dom'
+import { Table, Divider, Spin, Icon } from 'antd';
 
 import './ActiveCombo.scss'
+import { debounce } from '../../../utils';
+
+const VouchersDetail = ({ voucherIdArray, description }) => (
+    <>
+        <p>description: {description}</p> 
+        <ul>
+            {voucherIdArray.map((item, index) => <li key={index}>{item} x 14 detail....</li>)}
+        </ul>
+    </>
+)
+
+const Loading = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
 const ActiveCombo = ({ combos }) => {
+    const [isSearching, setIsSearching] = useState(false);
+
+    const [displayCombos, setDisplayCombos] = useState([]);
+    useEffect(() => {
+        setDisplayCombos(combos)
+        setSearch('')
+    }, [combos])
+    useEffect(() => {
+        setIsSearching(false);
+    }, [displayCombos])
+
+    const searchCombo = useCallback(
+        debounce(str => {
+            setDisplayCombos(combos.filter(combo => combo.description.includes(str)))
+        }, 300), [combos]
+    )
+
+    const [search, setSearch] = useState('');
+    useEffect(() => {
+        searchCombo(search)
+    }, [search, searchCombo])
+
+    const searchChange = e => {
+        setSearch(e.target.value)
+        setIsSearching(true)
+    }
+
+    const tableConfig = {
+        pagination: { position: 'top' },
+        size: 'small',
+        expandedRowRender: VouchersDetail,
+        scroll: { y: 450 },
+        rowKey: () => uuid()
+    }
     const columns = [
         {
+            key: 'id',
             title: 'ID',
             dataIndex: 'id',
-            key: uuid()
+            width: 50
         },
         {
+            key: 'name',
             title: 'Name',
             dataIndex: 'id',
-            key: uuid(),
-            render: id => `Name ${id}`
+            render: id => `Name ${id}`,
+            width: 100
         },
         {
+            key: 'fromDate',
             title: 'From',
             dataIndex: 'fromDate',
-            key: uuid()
+            width: 150,
+            sorter: (a, b) => new Date(a.id) - new Date(b.id)
         },
         {
+            key: 'toDate',
             title: 'To',
             dataIndex: 'toDate',
-            key: uuid()
+            width: 150,
+            sorter: (a, b) => new Date(a.id) - new Date(b.id)
         },
         {
-            title: 'Voucher',
+            key: 'voucherIdArray',
+            title: 'Vouchers',
             dataIndex: 'voucherIdArray',
-            key: uuid(),
             render: voucherIdArray => (
-                <ul>
-                    {voucherIdArray.map(item => <li>{item}</li>)}
+                <ul className="voucher-list">
+                    {voucherIdArray.map((item, index) => <li key={index}>{item} x 3</li>)}
                 </ul>
-                )
+            ),
+            width: 150
         },
         {
+            key: 'action',
             title: 'Action',
-            key: uuid(),
             render: (_, record) => (
                 <span>
-                    <span className="fake-link" >View Detail {record.id}</span>
+                    <Link to={`/a/combo/active/${record.id}`}>View Detail</Link>
                     <Divider type="vertical" />
                     <span className="fake-link">Stop</span>
                 </span>
-            )
+            ),
+            width: 200
         }
     ]
-    console.log(combos)
+
     return (
         <div className="active-combos">
-            <h1 >Active Combos</h1>
-            <hr />
+            <div className="header">
+                <h1 >Active Combos</h1>
+                <hr className="" />
+            </div>
             <div>
-                <span>search: </span>
-                <input />
+                <span>search: </span> 
+                <input value={search} onChange={searchChange} placeholder="enter combo name" />
+                { isSearching && <Spin indicator={Loading} />}
+
             </div>
             <div className="combo-list">
-                <Table rowKey={() => uuid()} dataSource={combos} columns={columns} />
+                <Table
+                    {...tableConfig}
+                    dataSource={(displayCombos.length > 0 ? displayCombos : null)}
+                    columns={columns}
+                />
             </div>
         </div>
     )
---
+++ src/components/Combo/ManageCombo/ManageCombo.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Demo = () => <div>xx </div>
+
+export default  Demo
