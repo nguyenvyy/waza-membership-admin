b2811aa5fb739c33ce4803176c2056083e833eef
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-18 22:01:11 +0700

    add voucher model

--- src/models/Combo.js
+++ src/models/Combo.js
@@ -1,14 +1,14 @@
 
 export class voucher_item {
-    constructor(voucherId, count = 1) {
-        this.voucherId = voucherId
+    constructor(voucher_id, count = 1) {
+        this.voucher_id = voucher_id
         this.count = count
     }
 }
 
 export class Combo {
-    constructor(id, combo_name, description, value, state, from_date, to_date, voucher_array = [], isDeleted = false, days = 30) {
-        this.id = id
+    constructor(_id, combo_name, description, value, state, from_date, to_date, voucher_array = [], isDeleted = false, days = 30) {
+        this._id = _id
         this.name = combo_name
         this.value = value
         this.state = state
@@ -18,6 +18,25 @@ export class Combo {
         this.voucherIdArray = voucher_array
         this.description = description
         this.days = days
+    }
+}
+
+export class Voucher {
+    constructor(_id, disscount, value, max_value, from_date, to_date, state, category, subcategory, times_to_use, isDeleted, description, voucher_name, point) {
+        this._id = _id
+        this.disscount = disscount
+        this.value = value
+        this.max_value = max_value
+        this.from_date = from_date
+        this.to_date = to_date
+        this.state = state
+        this.category = category
+        this.subcategory = subcategory
+        this.times_to_use = times_to_use
+        this.isDeleted = isDeleted
+        this.description = description
+        this.voucher_name = voucher_name
+        this.point = point
     }
 }
 
