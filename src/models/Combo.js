439b78f131f6c395261e8d948a30bd01a65828c5
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-20 15:00:05 +0700

    update combo  model

--- src/models/Combo.js
+++ src/models/Combo.js
@@ -9,13 +9,13 @@ export class voucher_item {
 export class Combo {
     constructor(_id, combo_name, description, value, state, from_date, to_date, voucher_array = [], isDeleted = false, days = 30) {
         this._id = _id
-        this.name = combo_name
+        this.combo_name = combo_name
         this.value = value
         this.state = state
-        this.fromDate = from_date
-        this.toDate = to_date
+        this.from_date = from_date
+        this.to_date = to_date
         this.isDeleted = isDeleted
-        this.voucherIdArray = voucher_array
+        this.voucher_array = voucher_array
         this.description = description
         this.days = days
     }
