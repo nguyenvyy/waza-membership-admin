073fd0c15805cad08450070b3918632e3fd711be
unknown <ngodongquan021098@gmail.com>
2019-10-20 18:36:35 +0700

    create Voucher

--- src/components/Voucher/CreateVoucher/CreateVoucher.js
+++ src/components/Voucher/CreateVoucher/CreateVoucher.js
@@ -1,33 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Radio } from 'antd';
 import { DatePicker, Select } from 'antd';
 import { Link } from 'react-router-dom'
 
 const  Option  = Select.Option;
 const CreateVoucher = () => {
+    const intialState = {
+        currentButton : 'Point',
+        currentType: 'Discount'
+    }
+
+    const [toggle, setToggle] = useState(intialState)
+
+    const changeCurrentButton = e => {
+        setToggle({
+            currentButton: e.target.value
+        })
+        console.log(toggle.currentButton)
+    }
+
+    const selectType = value => {
+        setToggle({
+            ...toggle,
+            currentType: value
+        })
+        console.log(toggle.currentButton)
+    }
+
     return (
         <div className="main-crvoucher">
             <h1>
               Voucher:
             </h1>
             <Radio.Group defaultValue="Point" buttonStyle="solid">
-                <Radio.Button value="Point">Point</Radio.Button>
-                <Radio.Button value="Gift">Gift</Radio.Button>
-                <Radio.Button value="Buy">Buy</Radio.Button>
+                <Radio.Button onClick={changeCurrentButton} value="Point">Point</Radio.Button>
+                <Radio.Button onClick={changeCurrentButton} value="Gift">Gift</Radio.Button>
+                <Radio.Button onClick={changeCurrentButton} value="Buy">Buy</Radio.Button>
             </Radio.Group>
 
             <div className="create-voucher">
                 <div className="create1">
                     <div className="content-create">
-                        <label>Name:</label>
+                        <label>Voucher Name:</label>
                         <input readOnly></input>
                     </div>
                     <div className="content-create">
-                        <label>Voucher Name:</label>
-                        <input></input>
-                    </div>
-                    <div className="content-create">
-                        <label>Point:</label>
+                        {toggle.currentButton === 'Point'?<label>Point:</label>: ''}
+                        {toggle.currentButton === 'Gift'?<label>Gift:</label>: ''}
+                        {toggle.currentButton === 'Buy'?<label>Buy:</label>: ''}
                         <input></input>
                     </div>
                     <div className="content-create">
@@ -46,22 +66,23 @@ const CreateVoucher = () => {
                 <div className="create1">
                     <div className="content-create">
                         <label>Status:</label>
-                        <input readOnly></input>
+                        <input defaultValue="Unavailable" readOnly></input>
                     </div>
                     <div className="content-create">
                         <label>Conditions Type:</label>
-                        <Select style={{ width: 120 }} defaultValue="discount" >
-                            <Option value="discount">Discount</Option>
-                            <Option value="value">Value</Option>
+                        <Select onChange={selectType} style={{ width: 120 }} defaultValue={toggle.currentType} >
+                            <Option value="Discount">Discount</Option>
+                            <Option value="Value">Value</Option>
+                            <Option value="Combine">Combine</Option>
                         </Select>
                     </div>
                     <div className="content-create">
                         <label>Value:</label>
-                        <input></input>
+                        {toggle.currentType === 'Discount' ?<input disabled></input> : <input></input>}
                     </div>
                     <div className="content-create">
                         <label>Discount:</label>
-                        <input></input>
+                        {toggle.currentType === 'Value' ? <input disabled></input> : <input></input>}
                     </div>
                     <div className="content-create">
                         <label>Max Cost:</label>
@@ -71,7 +92,8 @@ const CreateVoucher = () => {
                         <label>Sub Type:</label>
                         <Select style={{ width: 120 }} defaultValue="food" >
                             <Option value="food">Food</Option>
-                            <Option value="drink">Drink</Option>
+                            <Option value="drink">Move</Option>
+                            <Option value="drink">Shopping</Option>
                         </Select>
                     </div>
 
