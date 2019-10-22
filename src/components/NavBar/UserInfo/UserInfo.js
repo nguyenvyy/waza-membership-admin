ddb9037b008c650f0a0142435c6931b3ea724d7b
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-22 21:42:33 +0700

    add feature logout

--- src/components/NavBar/UserInfo/UserInfo.js
+++ src/components/NavBar/UserInfo/UserInfo.js
@@ -1,29 +1,55 @@
-import React from 'react'
-import { Icon, Tooltip } from 'antd'
+import React, {memo} from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { Icon, Tooltip, message } from 'antd'
 
 import './UserInfo.scss'
+import { requestLogout } from '../../../redux/actions/auth-actions/actions'
 
-export const UserInfo = ({collapsed, username = 'unknown'}) => (
-    <div className={`user-paner${collapsed ? '-collapsed' : ''}`}>
-    {
-        !collapsed ? (
-            <>
-                <div className="user-name ">
-                    <span>
-                        {username}
-                    </span>
-                </div>
-                <span className="pointer">
-                    <Icon type="logout" />
-                </span>
-            </>
-        ) : (
-            <Tooltip placement="right" title={'Logout! ' + username}>
-                <span>
-                    <Icon style={{fontSize: '20px'}} type="logout" />
-                </span>
-            </Tooltip>
+export const UserInfo = memo(
+    function UserInfo({ collapsed }) {
+        const dispatch = useDispatch();
+        const username = useSelector(state => state.user.info &&  state.user.info.username) || "anonymous"
+        const handleLogout = () => {
+            const hiden = message.loading('Logout...', 0)
+            dispatch(requestLogout()).then(res => {
+                switch (res.status) {
+                    case 200:
+                        message.success('Logout success',1)
+                        break;
+                    case 401: 
+                        message.warning('Your login session has expired ',1)
+                        break;
+                    default:
+                        message.error('Error', 1)
+                        break;
+                }
+                setTimeout(hiden, 100)
+            })
+        }
+        return (
+            <div className={`user-paner${collapsed ? '-collapsed' : ''}`}>
+                {
+                    !collapsed ? (
+                        <>
+                            <div className="user-name ">
+                                <span>
+                                    {username}
+                                </span>
+                            </div>
+                            <span onClick={handleLogout} className="pointer">
+                                <Icon type="logout" />
+                            </span>
+                        </>
+                    ) : (
+                            <Tooltip placement="right" title={'Logout! ' + username}>
+                                <span onClick={handleLogout}> 
+                                    <Icon style={{ fontSize: '20px' }} type="logout" />
+                                </span>
+                            </Tooltip>
+                        )
+                }
+            </div>
         )
     }
-</div>
 )
