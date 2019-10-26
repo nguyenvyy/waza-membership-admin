1a1f224510ecf8114f9a2f27e76f47c4afe46da4
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-26 14:47:09 +0700

    add validate utils

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,5 +1,16 @@
 
 
 export const checkPositiveNumber = value => /^(\d+\d+)$|^(\d+)$/gm.test(value)
-
 export const checkDivideBy = (number, by) => number%by === 0 ? true : false
+
+export const checkMinMax = (value, min, max) => {
+    return value >= min && value <= max
+}
+
+export const checkIsInterge = value => Number.isInteger(value)
+export const check = value => /^[0-9]*$/gm.test(value)
+export const checkIsNaN = value => isNaN(value);
+export const checkNoSymbolsOrSpecialChars = value => {
+    let regex = new RegExp(/^[_A-z0-9]*((-|\s)*[_A-z0-9])*$/g)
+    return regex.test(value)
+}
