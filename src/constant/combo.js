214c61b395833ab44b3cc31c0c9e6ac6481e1471
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-29 14:22:13 +0700

    change style error message

--- src/constant/combo.js
+++ src/constant/combo.js
@@ -34,6 +34,6 @@ export const errorMessage = {
     days: `The input must be an integer and from ${comboLimitValue.days.min} to ${comboLimitValue.days.max} day`,
     combo_name: `The input must be from ${comboLimitValue.combo_name.min} to ${comboLimitValue.combo_name.max} character and without special character`,
     value: `The input must be an integer, division by 1000 and from ${formatVND(comboLimitValue.value.min)} to ${formatVND(comboLimitValue.value.max)}`,
-    voucher_array: `Number of vouchers must be greater than ${comboLimitValue.voucher_array.min}`,
+    voucher_array: max => `Number of vouchers must be ${max}`,
     description: `The input must be from ${comboLimitValue.description.min} to ${comboLimitValue.description.max} character and without special character`,
 }
