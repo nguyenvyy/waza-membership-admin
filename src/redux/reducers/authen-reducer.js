20bdff7d3302d8ab8392353cc8eab03fb4269843
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-11-26 20:07:19 +0700

    delete auth

--- src/redux/reducers/authen-reducer.js
+++ src/redux/reducers/authen-reducer.js
@@ -1,16 +1,8 @@
 import { LOGIN, AUTHENTICATION_SUCCEEDED, STOP_LOGIN, LOGOUT } from "../actions/auth-actions/types"
 
 const initState = {
-    info: {
-        isDeleted: false,
-        _id: '5dac3ad4645b72001790ce47',
-        username: 'admin',
-        createdAt: '2019-10-20T10:45:40.205Z',
-        updatedAt: '2019-11-23T00:24:12.182Z',
-        __v: 255,
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGFjM2FkNDY0NWI3MjAwMTc5MGNlNDciLCJpYXQiOjE1NzQ0Njg2NTJ9.ZtiHsJ6YFBMHo1pROaVsw8snmQRQDozJOEGsFUlUcgo'
-    },
-    isLoggedIn: true,
+    info: null,
+    isLoggedIn: false,
     isLoadingUser: false,
 }
 
